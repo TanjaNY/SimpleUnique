d6f9fb99e8394c0839799d3817befa7e110ec547
Conrad Beckert <cb@miradata.de>
2020-07-15 16:47:55 +0000

    Zweite Version, die mehrere unterscheidbare Tokens pro Konto haben kann.
    dazu die Methode tokenOfOwnerByIndex implementiert

--- contracts/SimpleUnique.sol
+++ contracts/SimpleUnique.sol
@@ -1,106 +1,132 @@
 pragma solidity ^0.5.0;
 
 /*
-*    Simple Example of an unique non fungible ERC-721 token 
+*    Simple Example of an unique non fungible ERC-721 token
 *    https://medium.com/crypto-currently/the-anatomy-of-erc721-e9db77abfc24
 */
 
 
 contract SimpleUnique {
-    
+
     address owner;
-    mapping( address => uint ) private balances;                                /* wieviele Tokens hat die Adresse? */
-    mapping( uint256 => address ) private tokenOwners;                          /* wer hat das Token x ? */
-    mapping( uint256 => bool ) private tokenExists;                             /* gibt es das Token x ja/nein */
-    mapping( address => mapping ( uint256 => uint256 ) ) private ownerTokens;   /* welche Tokens sind auf dieser Adresse? */
-    mapping( address => mapping (address => uint256 )) allowed;                  /* darf sich das Token holen */
-    
-    
+    uint256 totalCoins;
+    //mapping( address => uint ) private balances;                                /* wieviele Tokens hat die Adresse? */
+    mapping( address => uint256[] ) private tokensPerOwner;
+    mapping( uint256 => address ) private whoHastoken;                          /* wer hat das Token x ? */
+    mapping( address => mapping ( address => uint256 )) private allowed;        /* darf sich das Token holen */
+
     constructor() public {
         owner = msg.sender;
+        totalCoins = 0;
     }
-    
-    
+
+    // --- Description ---
     function name() public pure returns (string memory) {
         return "NodeAndCode";
     }
-    
+
     function symbol() public pure returns (string memory) {
         return "NACU";
     }
-    
-    function totalSupply() public pure returns (uint256) {
-        return 1000;
+
+    function totalSupply() public view returns (uint256) {
+        return totalCoins;
     }
- 
- 
+
+
+    // --- My Tokens ---
     function balanceOf(address _owner) public view returns (uint) {
-        return balances[_owner];
+        return tokensPerOwner[_owner].length;
     }
-    
-    
+
+
+    // --- Who has a particular token ---
     function ownerOf(uint256 _tokenId) public view returns (address) {
-        require(tokenExists[_tokenId]);
-        
-        return tokenOwners[_tokenId];
+        require(whoHastoken[_tokenId] != address(0));
+
+        return whoHastoken[_tokenId];
     }
-    
-    
+
+    function tokenOfOwnerByIndex(address _owner, uint256 _idx) public view returns (uint256) {
+      return tokensPerOwner[_owner][_idx];
+    }
+
+
+    // --- Mint Token ---
     function mintToken(uint256 _tokenId) public {
         require( msg.sender == owner );
-        
-        balances[msg.sender] += 1;
-        tokenOwners[_tokenId] = msg.sender;
-        tokenExists[_tokenId] = true;
+        require( _tokenId > 0 );
+        require( whoHastoken[_tokenId] == address(0) );
+
+        tokensPerOwner[owner].push(_tokenId);
+        whoHastoken[_tokenId] = owner;
+        totalCoins++;
     }
-    
-    
+
+
+    // --- search token in owner's list ---
+    function _searchToken( address _owner, uint256 _tokenId ) private view returns (uint256) {
+        uint256 n = 0;
+        while (n < tokensPerOwner[_owner].length && tokensPerOwner[_owner][n] != _tokenId) {
+          n++;
+        }
+        if ( tokensPerOwner[_owner][n] == _tokenId ) {
+          return n;  // --- gefunden  ---
+        }
+        else {
+          return 0; // nicht gefunden
+        }
+    }
+
+    // --- Ownership Transfer ---
+    function _transfer( address _from, address _to, uint256 _tokenId ) private {
+      uint256 pos = _searchToken( _from, _tokenId);
+      uint256 cnt = tokensPerOwner[_from].length - 1;
+
+      require( tokensPerOwner[_from][pos] == _tokenId ); // --- gefunden  ---
+
+      tokensPerOwner[_from][pos] = tokensPerOwner[_from][cnt];
+      delete tokensPerOwner[_from][cnt];
+      tokensPerOwner[_from].length--;
+      tokensPerOwner[_to].push(_tokenId);
+
+      whoHastoken[_tokenId] = _to;
+
+      emit Transfer(_from, _to, _tokenId);
+    }
+
+    function transfer( address _to, uint256 _tokenId ) public {
+
+        address currentOwner = msg.sender;
+        address newOwner = _to;
+
+
+        require( whoHastoken[_tokenId] != address(0) );
+        require( newOwner != ownerOf(_tokenId) );
+        require( newOwner != address(0) );
+
+        _transfer(currentOwner, newOwner, _tokenId);
+    }
+
     function approve(address _to, uint256 _tokenId) public {
         require( msg.sender == ownerOf(_tokenId) );
         require( msg.sender != _to );
-        
+
         allowed[msg.sender][_to] = _tokenId;
         emit Approval(msg.sender, _to, _tokenId);
     }
-    
-    
+
+
     function takeOwnership(uint256 _tokenId) public {
-        address oldOwner = ownerOf(_tokenId);
+        address currentOwner = ownerOf(_tokenId);
         address newOwner = msg.sender;
-        
-        require( newOwner != oldOwner );
-        require( allowed[oldOwner][newOwner] == _tokenId );
-        balances[oldOwner] -= 1;
-        tokenOwners[_tokenId] = newOwner;
-        balances[newOwner] += 1;
-        emit Transfer(oldOwner, newOwner, _tokenId);
+
+        require( newOwner != currentOwner );
+        require( allowed[currentOwner][newOwner] == _tokenId );
+
+        _transfer(currentOwner,newOwner,_tokenId);
     }
-    
-    /* bislang nicht im Einsatz 
-    function removeFromTokenList(address _owner, uint256 _tokenId) private {
-        for(uint256 i = 0 ; ownerTokens[_owner][i] != _tokenId ; i++ ) {  
-            ownerTokens[_owner][i] = 0;
-        }
-    }
-    */
-    
-    function transfer( address _to, uint256 _tokenId ) public {
-        address currentOwner = msg.sender;
-        address newOwner = _to;
-        
-        require( tokenExists[_tokenId] );
-        require( newOwner != ownerOf(_tokenId) );
-        require( newOwner != address(0) );
-        
-        balances[currentOwner] -= 1;
-        tokenOwners[_tokenId] = newOwner;
-        
-        balances[newOwner] += 1;
-        emit Transfer(currentOwner, newOwner, _tokenId);
-    }
-    
-    
+
     event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
-    
     event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
 }
